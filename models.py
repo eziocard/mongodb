e2eaa6d0698dd3cc9af2abfdc5d4a185c834bd08
AlianNF <103971778+AlianNF@users.noreply.github.com>
2024-09-06 08:13:05 -0400

    Añadido actualizar y eliminar.

--- models.py
+++ models.py
@@ -65,4 +65,49 @@ def Filtrar(search,seleccion_2):
         print('TABLA FILTRADA')
         print(df.head())
 
+## funcion para actualizar registros
+
+def Actualizar(search,seleccion,campo,nuevo_valor):
+    #conexion a dbs
+    db = client.Universidad
+    #seleccion para estudiantes
+    if seleccion == 1:
+        collection = db['Estudiantes'] 
+        criterio = {'rut':search} #creamos un filtro(criterio) en forma de diccionario para proporcionarlo al comando update_one
+    elif seleccion == 2:
+        collection = db['Cursos']
+        criterio = {'codigo_curso':search}
+
+    #crear el nuevo campo
+    actualizacion = {'$set':{campo : nuevo_valor}}
+    #realizar la actualizacion
+    resultado = collection.update_one(criterio,actualizacion)
+    #comprobar actualizacion
+    if resultado.update_count > 0:
+        print(f"Registro perteneciente a {search} actualizado exitosamente")
+    else:
+        print(f"No se encotro un registro con {search}")
+
+##funcion para eliminar
+
+def eliminar(rut=None,codigo_curso=None):
+    #conexion a dbs
+    db = client.Universidad
+    if rut:
+        collection = db['Estudiantes']
+        criterio = {'rut' : rut}
+        resultado = collection.delete_one(criterio)
+
+        if resultado.deleted_count > 0:
+            print(f"Estudiante con RUT {rut} eliminado exitosamente")
+        else:
+            print(f"RUT: {rut} no encontrado")
+    elif codigo_curso:
+        collection = db['Cursos']
+        criterio = {'codigo_curso' : codigo_curso}
+        resultado = collection.delete_one(criterio)
+        if resultado.deleted_count > 0:
+            print(f"Curso con codigo {codigo_curso} eliminado exitosamente")
+        else:
+            print(f"Curso: {codigo_curso} no encontrado")
 
